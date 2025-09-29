49d08bc07d9e3c2514644a92e1b0f16763a1531e
balaji640 <rambalaji965@gmail.com>
2025-09-29 20:25:30 +0530

    Update BehavioralPattern_StrategyPattern.java

--- exercise_1/BehavioralPattern_StrategyPattern.java
+++ exercise_1/BehavioralPattern_StrategyPattern.java
@@ -6,12 +6,11 @@ import exercise_1.PayPalPayment;
 import exercise_1.PaymentStrategy;
 import exercise_1.ShoppingCart;
 
-//Strategy interface
+
 	interface PaymentStrategy {
 	 void pay(int amount);
 	}
 
-	//Concrete strategies
 	class CreditCardPayment implements PaymentStrategy {
 	 public void pay(int amount) { System.out.println("Paid " + amount + " using Credit Card."); }
 	}
@@ -19,14 +18,14 @@ import exercise_1.ShoppingCart;
 	 public void pay(int amount) { System.out.println("Paid " + amount + " using PayPal."); }
 	}
 
-	//Context
+
 	class ShoppingCart {
 	 private PaymentStrategy strategy;
 	 public void setPaymentStrategy(PaymentStrategy strategy) { this.strategy = strategy; }
 	 public void checkout(int amount) { strategy.pay(amount); }
 	}
 
-	//Demo
+
 	public class BehavioralPattern_StrategyPattern {
 	 public static void main(String[] args) {
 	     ShoppingCart cart = new ShoppingCart();
@@ -36,3 +35,4 @@ import exercise_1.ShoppingCart;
 	     cart.checkout(300);
 	 }
 	}
+
